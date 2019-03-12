7fbbc3496161080fa10d4b768da07e1a30e5755e
Nick Hoernle <nicholas.hoernle@gmail.com>
2019-03-12 18:02:32 +0200

    load new data from people who dont get silver

--- scripts/load_data.py
+++ scripts/load_data.py
@@ -83,7 +83,7 @@ def load_silver_editor_edits():
     return df
 
 def load_silver_editor_comments():
-    my_sql = f'''SELECT c.Id, c.UserId, c.CreationDate FROM comments as c
+    my_sql = f'''SELECT c.Id, c.PostId, c.UserId, c.CreationDate FROM comments as c
             INNER JOIN badges as b
             ON (
                 b.Name = 'Strunk & White' AND
@@ -94,3 +94,37 @@ def load_silver_editor_comments():
     file_name = '../data/df_silver_editor_comments.pkl'
     df = load_data_from_azure(file_name, my_sql)
     return df
+
+def load_not_silver_editers():
+    my_sql = f'''
+    SELECT ph2.UserId, COUNT(*) as count
+    FROM posthistory as ph2
+    WHERE ph2.UserId not in (SELECT b.UserId FROM Badges as b WHERE b.Name = 'Strunk & White') AND ph2.CreationDate < '2017-06-01'
+    GROUP BY UserId
+    HAVING COUNT(*) > 40;
+    LIMIT 5000;
+    '''
+    file_name = '../data/df_not_silver_editers.pkl'
+    df = load_data_from_azure(file_name, my_sql)
+    return df
+
+def load_not_silver_editors_edits(user_ids):
+    my_sql = f'''
+    SELECT ph.Id, ph.PostHistoryTypeId, ph.PostId, ph.UserId, ph.CreationDate
+    FROM posthistory as ph
+    WHERE ph.UserId in ({user_ids});
+    '''
+    file_name = '../data/df_not_silver_editors_edits.pkl'
+    df = load_data_from_azure(file_name, my_sql)
+    return df
+
+
+# ALTER TABLE badges ADD FOREIGN KEY (UserId) REFERENCES users(Id);
+# ALTER TABLE posthistory ADD FOREIGN KEY (UserId) REFERENCES users(Id);
+# ALTER TABLE posthistory ADD FOREIGN KEY (UserId) REFERENCES badges(UserId);
+# ALTER TABLE posthistory ADD FOREIGN KEY (PostId) REFERENCES posts(Id);
+# ALTER TABLE posthistory ADD FOREIGN KEY (PostId) REFERENCES posts(Id);
+
+{
+  "body": "{   \n    \"type\": \"notification_event\",\n    \"app_id\": \"anAppId\",\n    \"data\": {\n      \"type\": \"notification_event_data\",\n      \"item\": {\n        \"type\": \"conversation\",\n        \"id\": \"111111111\",\n        \"created_at\": 111111111,\n        \"updated_at\": 111111111,\n        \"user\": {\n          \"type\": \"user\",\n          \"id\": \"111111111\",\n          \"user_id\": \"111111111\",\n          \"name\": \"a name\",\n          \"email\": \"email@email.com\",\n          \"do_not_track\": null\n        },\n        \"assignee\": {\n          \"type\": \"nobody_admin\",\n          \"id\": null\n        },\n        \"conversation_message\": {\n          \"type\": \"conversation_message\",\n          \"id\": \"111111111\",\n          \"url\": null,\n          \"subject\": \"\",\n          \"body\": \"<p>I forgot my password</p>\",\n          \"author\": {\n            \"type\": \"user\",\n            \"id\": \"111111111\"\n          },\n          \"attachments\": []\n        },\n        \"conversation_parts\": {\n          \"type\": \"conversation_part.list\",\n          \"conversation_parts\": [],\n          \"total_count\": 0\n        },\n        \"open\": true,\n        \"state\": \"open\",\n        \"read\": true,\n        \"metadata\": {},\n        \"tags\": {\n          \"type\": \"tag.list\",\n          \"tags\": []\n        },\n        \"tags_added\": {\n          \"type\": \"tag.list\",\n          \"tags\": []\n        },\n        \"links\": {\n          \"conversation_web\": \"https://app.intercom.io/\"\n        }\n      }\n    },\n    \"links\": {},\n    \"id\": \"notif_something\",\n    \"topic\": \"conversation.user.created\",\n    \"delivery_status\": \"pending\",\n    \"delivery_attempts\": 1,\n    \"delivered_at\": 0,\n    \"first_sent_at\": 111111111,\n    \"created_at\": 111111111,\n    \"self\": null\n    }"
+}
