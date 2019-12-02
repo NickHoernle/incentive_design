b91aa6d722ab64b7efc1f4ae4ac5c42e44088b82
Nick Hoernle <nicholas.hoernle@gmail.com>
2019-12-02 13:17:55 +0000

    remove legacy folders and create src folder

---
+++ src/load_so_data.py
@@ -0,0 +1,308 @@
+import os
+from collections import namedtuple
+from urllib.request import urlopen
+import pickle
+
+import torch
+import numpy as np
+import pandas as pd
+import json
+
+ACTIONS = ['Answers', 'Questions', 'Comments', 'Edits', 'AnswerVotes', 'QuestionVotes', 'ReviewTasks']
+BADGES = ['CivicDuty', 'CopyEditor', 'Electorate', 'Reviewer', 'Steward', 'StrunkWhite']
+action_ixs = {a:i for i,a in enumerate(ACTIONS)}
+
+csv_path = '/Users/nickhoernle/edinburgh/incentive_design/data/'
+
+'''
+Scripts that transform the raw CSV data from stack overflow into a data folder that contains
+.pt array.
+folder/
+|---- src/
+|--------- scripts.py
+|---- data/
+|--------- badge_achievements.json
+|--------- data_indexes.json
+|--------- user_{id}.pt
+'''
+
+from torch.utils import data
+
+class StackOverflowDataset(data.Dataset):
+
+    def __init__(self, data_path='../data', dset_type='train', badge_focus='Electorate', subsample=5000, centered=True, window_length=70, input_length='full', out_dim=None):
+        '''
+        Initialise the stack overflow data loader
+        :param list_IDs: list of ids that belong to this dataset
+        :param badge_ids: dictionary of badges achieved
+        :param badge_focus: focus on a particular badge
+        :param subsample: subsample the data to smaller fraction
+        :param centered: to center the time series around the badge achievement
+        :param window_length: length of window around badge point
+        :param dset_type: can be "train", "test", "validate"
+        :param input_length: 'full' or length specifying the length of the original time series to use in the
+                             inference network (encoding) step
+        :param out_dim: dimension of the output to use, if None return the whole array, if string then return the
+                        string must be a specific action type.
+        '''
+
+        with open("{}/badge_achievements.json".format(data_path), 'r') as f:
+            badge_ids = json.load(f)
+        with open("{}/data_indexes.json".format(data_path), 'r') as f:
+            list_IDs = json.load(f)
+        self.badge_focus = badge_focus
+        self.subsample = subsample
+        self.centered = centered
+        self.window_length = window_length
+        self.dset_type = dset_type
+
+        if input_length == 'full':
+            self.input_length = self.window_length * 2
+        else:
+            self.input_length = input_length
+
+        if out_dim == None:
+            self.out_dim = [0,1,2,3,4,5,6]
+        elif type(out_dim) == str:
+            self.out_dim = action_ixs[out_dim]
+        else:
+            self.out_dim = out_dim
+
+        self.list_IDs, self.badge_ids = self._preprocess_user_ids(list_IDs[dset_type], badge_ids)
+        self.data_shape = (self.input_length, len(ACTIONS))
+
+    def _preprocess_user_ids(self, list_IDs, badge_ids):
+        feasible_users = []
+        feasible_badges = {}
+        for user in list_IDs:
+            user = str(user)
+            if self.badge_focus in badge_ids[user]:
+                feasible_users.append(user)
+                feasible_badges[user] = int(np.random.choice(badge_ids[user][self.badge_focus], size=1))
+
+        assert len(feasible_users) >= self.subsample
+        return np.random.choice(feasible_users, size=self.subsample, replace=False), feasible_badges
+
+    def __data_trans_in(self, data):
+        data[data > 0] = 1
+        # return torch.log1p(data.float())
+        return data.float()
+
+    def __data_trans_out(self, data):
+        data[data > 0] = 1
+        return data.float()
+
+    def __len__(self):
+        return len(self.list_IDs)
+
+    def __get_prox_to_badge(self, output, badge_index):
+        prox_to_badge = torch.cumsum(output[:, self.out_dim], dim=0).float()
+        prox_to_badge = (prox_to_badge + (500 - prox_to_badge[badge_index])) / 500
+
+        prox_to_badge[badge_index + 1:] = 0
+        prox_to_badge[prox_to_badge < 0] = 0
+        prox_to_badge = prox_to_badge - prox_to_badge[0]
+        prox_to_badge[badge_index + 1:] = 0
+        prox_to_badge[prox_to_badge < 0] = 0
+
+        return prox_to_badge
+
+    def __getitem__(self, index):
+        # Select sample
+        ID = self.list_IDs[index]
+        badge_index = self.badge_ids[ID]
+
+        # Load data and get label
+        X = torch.load('data/user_' + ID + '.pt')
+        output = torch.zeros(size=(2*self.window_length, X.size()[1]))
+
+        if self.centered:
+            center = badge_index
+            # correct where the badge was achieved
+            badge_index = self.window_length
+
+        else:
+            # prob_of_center = generate_linear_selection_probabilities(len(X), badge_index, self.window_length)
+            start = np.max((0, badge_index-self.window_length+1))
+            stop = np.min((badge_index+self.window_length-1, X.size()[0]))
+
+            center = np.random.choice(np.arange(start, stop))
+            if badge_index < center:
+                badge_index = (self.window_length - (center-badge_index))
+            else:
+                badge_index = (badge_index - center) + self.window_length
+
+        if center < self.window_length:
+            output[self.window_length - center:] = X[0:center + self.window_length]
+        elif X.size()[0] - self.window_length < center:
+            diff = X.size()[0] - center
+            output[:self.window_length + diff] = X[center - self.window_length:]
+        else:
+            output = X[center - self.window_length:center + self.window_length, :]
+
+        x_in = self.__data_trans_in(output[:self.input_length,:].clone())
+        x_out = self.__data_trans_out(output[:,self.out_dim].clone())
+
+        # get the proximity to a badge
+        prox_to_badge = self.__get_prox_to_badge(output, badge_index)
+
+        # create the kernel that indexes the badge beung accepted at index=2*window_length
+        kernel_data = np.arange(0, 4*self.window_length)
+
+        start = 2*self.window_length-badge_index
+        stop = 4*self.window_length-badge_index
+
+        return (x_in,
+                torch.tensor(kernel_data[start: stop]).view(-1).float(),
+                x_out,
+                prox_to_badge.view(-1,).float(),
+                torch.tensor(badge_index, dtype=torch.float))
+
+
+def generate_linear_selection_probabilities(length_of_seq, day_of_badge, window_length):
+    traj = -np.abs(np.arange(0, length_of_seq) - day_of_badge)
+    traj -= np.max(traj)
+    traj += window_length
+
+    # don't pick anything in the window length
+    traj[traj<0] = 0
+
+    # normalise
+    traj = traj / np.sum(traj)
+    return traj
+
+
+######################################################################################################################
+######################################################################################################################
+###################################### LEGACY AND FOR INITIAL DATA TRANSFORMATION ####################################
+######################################################################################################################
+######################################################################################################################
+
+def process_data(base_path):
+    # processed_dataset = {}
+    # validation == 1000 samples
+    # train === 5000 samples
+    # test === 1000 samples
+    # convert to number of actions per week
+    # edit out the badge outcome variables
+
+    print("Processing raw data")
+
+    output_fname = os.path.join(base_path, 'so_data.pkl')
+
+    labels = ['train', 'valid', 'test']
+
+    input_fname = os.path.join(csv_path, 'so_badges.csv')
+    data = pd.read_csv(input_fname)
+    data.Date = pd.to_datetime(data.Date)
+    data['week'] = (data.Date - pd.datetime(year=2017, month=1, day=1)).dt.days
+
+    data = data.groupby(['DummyUserId', 'week']).agg('sum').reset_index()
+    badge_ixs = data[data.Electorate > 0]
+    max_week = data.week.max()
+    badge_ixs = badge_ixs[badge_ixs.week > 45]
+    badge_ixs = badge_ixs[badge_ixs.week < max_week - 46]
+    badge_ixs = badge_ixs.DummyUserId
+
+    print(len(badge_ixs.unique()))
+
+    indexes = badge_ixs.unique()
+    train = np.random.choice(indexes, size=4000, replace=False)
+    indexes = indexes[~np.in1d(indexes, train)]
+    validate = np.random.choice(indexes, size=1000, replace=False)
+    indexes = indexes[~np.in1d(indexes, validate)]
+    test = np.random.choice(indexes, size=1000, replace=False)
+
+    # data.set_index('DummyUserId', inplace=True)
+    processed_dataset = {}
+
+    for s, dset in enumerate([train, validate, test]):
+
+        split = labels[s]
+        processed_dataset[split] = {}
+
+        sub_data = data[data.DummyUserId.isin(dset)]
+        n_seqs = len(dset)
+
+        processed_dataset[split]['sequence_lengths'] = torch.zeros(n_seqs, dtype=torch.long)
+        processed_dataset[split]['sequences'] = []
+        processed_dataset[split]['outcomes'] = []
+        idx = 0
+
+        for u_id, seqs in sub_data.groupby('DummyUserId'):
+            seqs = seqs.sort_values('week')
+
+            out = {}
+            for b in BADGES:
+                idxs = np.where(seqs[b] == 1)[0]
+                if len(idxs) > 0:
+                    out[b] = torch.tensor(idxs, dtype=torch.long)
+
+            civic_duty = out['Electorate']
+            days = 90
+
+            action_vec = seqs[ACTIONS].values[civic_duty - days // 2:civic_duty + days // 2, :]
+            out['Electorate'] = torch.tensor([days // 2], dtype=torch.long)
+
+            processed_dataset[split]['sequence_lengths'][idx] = days
+            processed_sequence = torch.tensor(action_vec, dtype=torch.long)
+            processed_dataset[split]['sequences'].append(processed_sequence)
+
+            processed_dataset[split]['outcomes'].append(out)
+            idx += 1
+
+    pickle.dump(processed_dataset, open(output_fname, "wb"), pickle.HIGHEST_PROTOCOL)
+    print("dumped processed data to %s" % output_fname)
+
+
+def load_data(base_path):
+    output_fname = os.path.join(base_path, 'so_data.pkl')
+    if not os.path.exists(output_fname):
+        process_data(base_path)
+
+    return pickle.load(open(output_fname, 'rb'))
+
+
+def transform_data_to_file_folder_structure(path_to_csv, path_to_data_dir):
+    data = pd.read_csv(path_to_csv)
+    data.Date = pd.to_datetime(data.Date)
+    data['day'] = (data.Date - pd.datetime(year=2017, month=1, day=1)).dt.days
+
+    data = data.groupby(['DummyUserId', 'day']).agg('sum').reset_index()
+    user_ids = data.DummyUserId.unique()
+    size_data = len(user_ids)
+    # print(int(np.floor(0.6*size_data)))
+
+    np.random.seed(11)
+
+    train = np.random.choice(user_ids, size=int(np.floor(0.6 * size_data)), replace=False)
+    user_ids = user_ids[~np.in1d(user_ids, train)]
+    validate = np.random.choice(user_ids, size=int(np.floor(0.2 * size_data)), replace=False)
+    user_ids = user_ids[~np.in1d(user_ids, validate)]
+    test = np.random.choice(user_ids, size=int(np.floor(0.2 * size_data)), replace=False)
+    # print(len(user_ids))
+
+    badge_achievements = {}
+
+    for dset in [train, validate, test]:
+        for user, trajectory in data[data.DummyUserId.isin(dset)].groupby('DummyUserId'):
+            trajectory = trajectory.sort_values('day')
+            badge = {}
+            for b in BADGES:
+                idxs = np.where(trajectory[b] == 1)[0]
+                if len(idxs) > 0:
+                    badge[b] = [int(i) for i in idxs]
+            badge_achievements[user] = badge
+
+            action_trajectory = torch.tensor(trajectory[ACTIONS].values, dtype=torch.long)
+            torch.save(action_trajectory, '{}/user_{}.pt'.format(path_to_data_dir, user))
+
+    with open('{}/badge_achievements.json'.format(path_to_data_dir), 'w') as f:
+        json.dump(badge_achievements, f)
+
+    with open('{}/data_indexes.json'.format(path_to_data_dir), 'w') as f:
+        obj = {}
+        obj['train'] = [int(u) for u in train]
+        obj['test'] = [int(u) for u in test]
+        obj['validate'] = [int(u) for u in validate]
+        json.dump(obj, f)
---
+++ src/main.py
@@ -0,0 +1,266 @@
+#!/usr/bin/env python
+
+import numpy as np
+import torch
+import torch.nn as nn
+from sklearn.preprocessing import MinMaxScaler
+import pickle
+from torch.utils.data import TensorDataset, DataLoader
+from tqdm import tqdm
+import models
+from torch.nn import functional as F
+
+import load_so_data as so_data
+from torch.backends import cudnn
+
+
+# CUDA for PyTorch
+use_cuda = torch.cuda.is_available()
+device = torch.device("cuda:0" if use_cuda else "cpu")
+cudnn.benchmark = True
+
+
+def main(args):
+
+    # Loading Parameters
+    params = {'batch_size': 256, 'shuffle': True, 'num_workers': 20}
+
+    max_epochs = 250
+    PRINT_NUM = 50
+    learning_rate = 1e-3
+    weight_decay = 1e-5
+    window_len = 5*7
+
+    common_params = {
+        'window_length': window_len,
+        'badge_focus': 'Electorate',
+        'out_dim': 'QuestionVotes',
+        'data_path': 'data',
+    }
+
+    dset_train = so_data.StackOverflowDataset(dset_type='train', subsample=4000, centered=False,
+                                              **common_params)
+    # dset_test = so_data.StackOverflowDataset(dset_type='test', subsample=1000,
+    #                                          **common_params)
+    dset_valid = so_data.StackOverflowDataset(dset_type='validate', subsample=1000, centered=False,
+                                              **common_params)
+
+    train_loader = DataLoader(dset_train, **params)
+    valid_loader = DataLoader(dset_valid, **params)
+
+    model_to_test = {
+        'baseline': models.BaselineVAE,
+        'linear': models.LinearParametricVAE,
+        'personalised_linear': models.LinearParametricPlusSteerParamVAE,
+        'full_parameterised': models.FullParameterisedVAE,
+        'full_personalised_parameterised': models.FullParameterisedPlusSteerParamVAE,
+        # 'flexible_linear': models.FlexibleLinearParametricVAE,
+        # 'personalised_flexible_linear': models.FlexibleLinearPlusSteerParamVAE,
+    }
+
+    dset_shape = dset_train.data_shape
+
+    for name, model_class in model_to_test.items():
+
+        model = model_class(obsdim=dset_shape[0]*dset_shape[1], outdim=dset_shape[0], device=device, proximity_to_badge=True).to(device)
+        optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+        loss = lambda x1,x2,x3,x4: models.BCE_loss_function(x1,x2,x3,x4, data_shape=dset_shape, act_choice=5)
+
+        print("Training model for: {}".format(name))
+        model = train_model(model, train_loader, valid_loader, optimizer, loss, NUM_EPOCHS=max_epochs, PRINT_NUM=PRINT_NUM)
+
+        print("Done")
+        print("Saving model into ../models/{}".format(name))
+        torch.save(model.state_dict(), "../models/{}.pt".format(name))
+
+def train_model(model, train_loader, valid_loader, optimizer, loss_fn, NUM_EPOCHS, PRINT_NUM=25):
+
+    for i in tqdm(np.arange(NUM_EPOCHS)):
+        model.train()
+        train_loss = 0
+
+        for train_in, kernel_data, train_out, train_prox, badge_date in train_loader:
+            optimizer.zero_grad()
+            # Transfer to GPU
+            train_in, kernel_data, train_out, train_prox, badge_date = train_in.to(device), kernel_data.to(device), train_out.to(device), train_prox.to(device), badge_date.to(device)
+
+            # Model computations
+            recon_batch, mu, logvar = model(train_in, kernel_data=kernel_data, dob=badge_date, prox_to_badge=train_prox)
+
+            loss = loss_fn(recon_batch, train_out, mu, logvar)
+            # loss = 100*torch.sum(model.badge_param.pow(2))
+            loss.backward()
+
+            optimizer.step()
+
+            # model.badge_param.data.clamp_(0)
+            # print(torch.max(model.badge_param.grad))
+            # for par in model.parameters():
+            #     print(torch.max(par.grad))
+            #     print(torch.max(par))
+            # print()
+
+            train_loss += loss.item()
+
+        # if i%PRINT_NUM==0:
+
+
+            # model.eval()
+
+        validation_loss = 0
+        for val_in, kernel_data, val_out, val_prox, badge_date in valid_loader:
+            # Transfer to GPU
+            val_in, kernel_data, val_out, val_prox, badge_date = val_in.to(device), kernel_data.to(device), val_out.to(device), val_prox.to(device), badge_date.to(device)
+
+                # Model computations
+                # Model computations
+            recon_batch, mu, logvar = model(val_in, kernel_data=kernel_data, dob=badge_date, prox_to_badge=val_prox)
+
+            loss = loss_fn(recon_batch, val_out, mu, logvar)
+            validation_loss += loss.item()
+
+            loss.backward()
+
+            optimizer.step()
+
+        if i % PRINT_NUM == 0:
+            # model.train()
+            print('====> Epoch: {} Average Valid loss: {:.4f}'.format(i, validation_loss/len(valid_loader.dataset)))
+
+    return model
+
+
+
+
+badge_threshold = 500
+badge_type = "Electorate"
+l1_loss = torch.nn.L1Loss(reduction='sum')
+
+def build_data(data):
+    train_x, val_x, test_x = [], [], []
+    train_y, val_y, test_y = [], [], []
+    test_yT = {}
+    day_of_badge = {}
+
+    for label, dset, outcome in tqdm(zip(['train', 'valid', 'test'],
+                                                  [train_x, val_x, test_x],
+                                                  [train_y, val_y, test_y])):
+
+        seqs = [s.numpy() for s in data[label]['sequences']]
+        badges = data[label]['outcomes']
+        mask = np.ones(90).astype(bool)
+
+        seq_shape = list(seqs[0].shape)
+        seq_shape[0] = np.sum(mask)
+
+        input_vecs = np.zeros(shape=(len(seqs), np.sum(mask), seq_shape[1]))
+        proximity = np.zeros(shape=(len(seqs), np.sum(mask)))
+
+        for i, seq in enumerate(seqs):
+            if badge_type in badges[i]:
+                badge_day = 45
+                acts_for_badge = seq[:, 5]
+
+                cs = np.cumsum(acts_for_badge)
+                cs[45:] += 1
+                cs[46:] += 1
+
+                proximity[i] = cs[mask]
+                proximity[i] = (proximity[i] - cs[45]) + badge_threshold
+                proximity[i][proximity[i] < 0] = 0
+                proximity[i][proximity[i] > badge_threshold] = 0
+
+            input_vecs[i] = np.array(seq)[mask]
+
+
+        sequence_data = input_vecs
+
+        lookback = 56
+        offset = (seq_shape[0] - lookback) // 7
+
+        inputs = np.zeros((len(sequence_data) * offset, lookback, seq_shape[1]))
+        outputs = np.zeros((len(sequence_data) * offset, 7))
+        prox_to_badge = np.zeros((len(sequence_data) * offset, lookback))
+        badge_day = np.zeros(len(sequence_data)* offset)
+
+        for i, seq in enumerate(sequence_data):
+            for j in np.arange(lookback, seq_shape[0] - 7, 7):
+                #             print(i*offset + (j - lookback)//7)
+                inputs[i * offset + (j - lookback) // 7] = seq[j - lookback:j, [0, 1, 2, 3, 4, 5, 6]]
+                outputs[i * offset + (j - lookback) // 7] = seq[j:j + 7, 5] > 1
+                prox_to_badge[i * offset + (j - lookback) // 7] = proximity[i][j - lookback:j]
+                badge_day[i * offset + (j - lookback) // 7] = 45 - (j-lookback)
+                #         inputs[(i * offset + (j - lookback))] = seq[j - lookback: j, [0,1,2,3,6]]
+                #         labels[(i * offset + (j - lookback))] = (input_vecs[i, j, 5]) > 0
+                #         prox_to_badge[(i * offset + (j - lookback))] = proximity[i, j - lookback: j]
+
+        dset.append(inputs)
+        outcome.append(outputs)
+        test_yT[label] = prox_to_badge
+        day_of_badge[label] = badge_day.astype(int)
+
+    return train_x[0], test_yT['train'], day_of_badge['train'], test_x[0], test_yT['test'], day_of_badge['test']
+
+
+def test(model, device, test_loader, loss, dset_shape):
+
+    model.eval()
+    test_loss = 0
+
+    with torch.no_grad():
+        for i, (data, prox, dob,) in enumerate(test_loader):
+            data = data.to(device)
+            prox = prox.to(device)
+            recon_batch, mu, logvar = model(data, prox, dob)
+
+            test_loss += loss(recon_batch, data, mu, logvar, dset_shape[1], dset_shape[2]).item()
+
+    test_loss /= len(test_loader.dataset)
+    print('====> Test set loss: {:.4f}'.format(test_loss))
+
+
+def test_on_votes_only(model, loader, dset_shape, device):
+
+    model.eval()
+    test_loss, pred_len = 0, 0
+    true_num_1 = 0
+    true_num_pos = 0
+    pred_pos_len = 0
+    false_num_pos = 0
+    total_pred_pos = 0
+    log_likelihood = 0
+
+    with torch.no_grad():
+        for i, (data, prox, dob) in enumerate(loader):
+            data = data.to(device)
+            prox = prox.to(device)
+            recon_batch, mu, logvar = model(data, prox, dob)
+
+            pp = recon_batch.cpu().detach().numpy().reshape(-1, dset_shape[1])
+            pred = (recon_batch.cpu().detach().numpy().reshape(-1, dset_shape[1]) > 0.5).astype(int)
+            true = (data.detach().cpu().numpy().reshape(-1, dset_shape[1], dset_shape[2])).astype(int)
+            true[true > 0] = 1
+
+            for t1, p1, pp_ in zip(true, pred, pp):
+                log_likelihood += np.sum(np.log(pp_) * t1[:, 5] + np.log(1 - pp_) * (1 - t1[:, 5] ))
+                true_num_1 += np.sum(t1[:,5])
+                test_loss += np.sum(p1 == t1[:,5])
+
+                pred_len += len(p1)
+
+                true_num_pos += np.sum(t1[:, 5][t1[:, 5] == 1] == p1[t1[:, 5] == 1])
+                pred_pos_len += np.sum(t1[:, 5] == 1)
+
+                false_num_pos += np.sum(t1[:, 5][p1 == 1])
+                total_pred_pos += np.sum(p1 == 1)
+
+
+    print('====> Action specific accuracy: {:.4f}'.format(test_loss/pred_len))
+    print('====> Recall: {:.4f}'.format(true_num_pos / pred_pos_len))
+    print('====> Precision: {:.4f}'.format(false_num_pos / total_pred_pos))
+    print('====> Log-likelihood: {:.4f}'.format(log_likelihood/(i*len(pp_))))
+    print('====> Baseline count: {:.4f}'.format(true_num_1 / pred_len))
+    print()
+
+if __name__ == "__main__":
+    main([])
