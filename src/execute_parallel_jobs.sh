a8ed3241edc14949abc117c25429ac945db5f2d3
Nick Hoernle <nicholas.hoernle@gmail.com>
2020-04-03 09:23:15 +0100

    fix type

--- src/execute_parallel_jobs.sh
+++ src/execute_parallel_jobs.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-EXPT_FILE=experiments.txt
+EXPT_FILE=experiment.txt
 NR_EXPTS=`cat ${EXPT_FILE} | wc -l`
 MAX_PARALLEL_JOBS=15
 sbatch --array=1-${NR_EXPTS}%${MAX_PARALLEL_JOBS} run_script.sh $EXPT_FILE
