ca8381fdef6e68e2180a031a4a3fa32010388813
Nick Hoernle <nicholas.hoernle@gmail.com>
2020-04-03 09:21:08 +0100

    get ready for running experiment

--- src/execute_parallel_jobs.sh
+++ src/execute_parallel_jobs.sh
@@ -2,5 +2,5 @@
 
 EXPT_FILE=experiments.txt
 NR_EXPTS=`cat ${EXPT_FILE} | wc -l`
-MAX_PARALLEL_JOBS=10
+MAX_PARALLEL_JOBS=15
 sbatch --array=1-${NR_EXPTS}%${MAX_PARALLEL_JOBS} run_script.sh $EXPT_FILE
