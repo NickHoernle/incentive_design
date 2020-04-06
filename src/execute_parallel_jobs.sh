1df608f27e558478f32a4ae290b35990e40ff839
Nick Hoernle <nicholas.hoernle@gmail.com>
2020-04-06 10:39:39 +0100

    increase the number of allowed parallel jobs

--- src/execute_parallel_jobs.sh
+++ src/execute_parallel_jobs.sh
@@ -2,5 +2,5 @@
 
 EXPT_FILE=experiment.txt
 NR_EXPTS=`cat ${EXPT_FILE} | wc -l`
-MAX_PARALLEL_JOBS=5
+MAX_PARALLEL_JOBS=15
 sbatch --array=1-${NR_EXPTS}%${MAX_PARALLEL_JOBS} run_script.sh $EXPT_FILE
