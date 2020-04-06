e9d9c860fb6f2ec26e7242e4bb47ff0447777c2f
Nick Hoernle <nicholas.hoernle@gmail.com>
2020-04-06 10:38:22 +0100

    dramatically increase the early stopping limits

--- src/gen_experiments.py
+++ src/gen_experiments.py
@@ -11,17 +11,17 @@ SCRATCH_HOME = f'{SCRATCH_DISK}/{USER}'
 
 DATA_HOME = f'{SCRATCH_HOME}/incentive_design/data'
 base_call = (f"python main_strunk_white_count_data.py --input {DATA_HOME} --output {DATA_HOME}/output "
-             "--epochs 2000 --early-stopping-lim 25 --model_name fp_flow_count --quiet")
+             "--epochs 2000 --early-stopping-lim 250 --model_name full_personalised_normalizing_flow --quiet")
 
 repeats = 3
-learning_rates = [1e-3, 1e-4, 1e-5, 1e-6]
+learning_rates = [1e-2, 1e-3, 1e-4]
 gammas = [.6, .7, .8, .9, .99]
 
 settings = [(lr, gam, rep) for lr in learning_rates for gam in gammas
             for rep in range(repeats)]
 nr_expts = len(learning_rates) * len(gammas) * repeats
 
-nr_servers = 10
+nr_servers = 15
 avg_expt_time = 45  # mins
 print(f'Total experiments = {nr_expts}')
 print(f'Estimated time = {(nr_expts / nr_servers * avg_expt_time)/60} hrs')
